0811f1035525709ea752695452ed5f40e1f6bef7
AndreyMazer <pyshnograyandrey@yandex.ru>
2024-01-13 13:30:00 +0300

    1301

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,7 +13,7 @@ import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Error from '../Error/Error';
 import Menu from '../Menu/Menu';
-import api from '../../utils/Api';
+import Api from '../../utils/Api';
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import InfoTooltip from '../InfoTooltip/InfoTooltip';
 import { register, authorize, checkToken } from '../../utils/Authorization';
@@ -45,7 +45,7 @@ function App() {
 
   React.useEffect(() => {
     if (loggedIn) {
-      Promise.all([api.getUser(), Api.getMovies()])
+      Promise.all([Api.getUser(), Api.getMovies()])
         .then(([resUser, resMovie]) => {
           setCurrentUser(resUser);
           setSavedMovies(resMovie);
