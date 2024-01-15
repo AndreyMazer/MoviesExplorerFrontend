1e9f92f7e8dccc534547533c3bd9660be1d67e4a
AndreyMazer <pyshnograyandrey@yandex.ru>
2024-01-15 19:49:49 +0300

    1501

--- src/components/InfoTooltip/InfoTooltip.js
+++ src/components/InfoTooltip/InfoTooltip.js
@@ -4,7 +4,7 @@ import errorPic from "../../images/neverno.svg";
 
 function InfoTooltip(props) {
     return (
-        <div className={`popup ${props.isOpen ? 'popup_opened' : ''}`}>
+        <div className={`popup popup_type_modal ${props.isOpen ? 'popup_opened' : ''}`}>
             <div className="popup__infotooltip">
                 <img className="popup__regImage" src={props.status ? okPic : errorPic} alt="результат" />
                 <h2 className="popup__feedback">{props.status ? props.succes : "Что-то пошло не так... Повоторите попытку снова"}</h2>
