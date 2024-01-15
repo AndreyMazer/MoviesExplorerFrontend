ebe87728eea36bb9c9a1653f800666eff7f9bd75
AndreyMazer <pyshnograyandrey@yandex.ru>
2024-01-15 22:03:37 +0300

    1501

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -24,12 +24,12 @@ function Login({ onLogin, errorMessage }) {
                 <form className="authorization__form" onSubmit={handleSubmit} noValidate>
                     <div className="authorization__label">
                         <p className="authorization__text">E-mail</p>
-                        <input value={values.email || ""} type="email" placeholder="Email" onChange={handleChange} pattern="^([^ ]+@[^ ]+\.[a-z]{2,6}|)$" className="authorization__input authorization__input_type_email" id="email-input" required />
+                        <input value={values.email || ""} type="email" name="email" placeholder="Email" onChange={handleChange} pattern="^([^ ]+@[^ ]+\.[a-z]{2,6}|)$" className="authorization__input authorization__input_type_email" id="email-input" required />
                         <span className={`authorization__text-error email-input-error ${errors.email ? "authorization__text-error_active" : ""}`}>{errors.email}</span>
                     </div>
                     <div className="authorization__label">
                         <p className="authorization__text">Пароль</p>
-                        <input value={values.password || ""} onChange={handleChange} type="password" placeholder="Пароль" className="authorization__input authorization__input_type_password" id="password-input" minLength="2" maxLength="200" required />
+                        <input value={values.password || ""} onChange={handleChange} type="password" name="password" placeholder="Пароль" className="authorization__input authorization__input_type_password" id="password-input" minLength="2" maxLength="200" required />
                         <span className={`authorization__text-error password-input-error ${errors.password ? "authorization__text-error_active" : ""}`}>{errors.password}</span>
                     </div>
                     <div className="authorization__button-label authorization__button-login">
